bf8ec7d2d807d0b0647a4d295203e9f92df5f592
sairaju225 <sairaju.atukuri225@gmail.com>
2021-08-02 12:14:57 +0530

    first commit-added (1) flask app (2) templates

---
+++ README.txt
@@ -0,0 +1,8 @@
+# Virtual environment for the flask app
+    flaskapp
+
+# To work on Virtual environment 
+    workon flaskapp
+
+# to deactivate virtual environment
+    deactivate
---
+++ main.py
@@ -0,0 +1,17 @@
+# An object of WSGI application
+from flask import Flask, render_template
+from flask import redirect, url_for, request
+# Flask constructor
+app = Flask(__name__)
+
+# Which url i want to access
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
